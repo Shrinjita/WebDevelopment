086cb6de3fdedf34cd749ed0f937aec75052023a
Shrinjita Paul <shrinjitapaul@gmail.com>
2024-02-28 01:04:10 +0530

    UPDATE

--- api/main.py
+++ api/main.py
@@ -2,21 +2,10 @@ import json
 
 from flask import Flask, render_template, jsonify
 
-app = Flask(__name__)
-
-def home():
+app = Flask(__name__, static_url_path='/static')
+@app.route('/')
+def serve_home():
     return render_template('front.html')
-
-def get_data():
-    with open('static/data/front.json', 'r') as f:
-        data = json.load(f)
-
-    # Access specific keys using the get function
-    user_images = data.get('userImages')
-    doctors = data.get('doctors')
-
-    return jsonify({'userImages': user_images, 'doctors': doctors})
-
 # Function mapping for routes
 @app.route('/')
 def serve_home():
@@ -25,3 +14,15 @@ def serve_home():
 @app.route('/data')
 def serve_data():
     return get_data()
+def get_data():
+    with open('WebDevelopment/static/data/front.json', 'r') as f:
+        data = json.load(f)
+
+    # Access specific keys using the get function
+    user_images = data.get('userImages')
+    doctors = data.get('doctors')
+
+    return jsonify({'userImages': user_images, 'doctors': doctors})
+
+if __name__ == "__main__":
+    app.run(debug=True)
