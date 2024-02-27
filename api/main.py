7d635580d668210d3a8f28399fac3e277c85d079
Shrinjita Paul <shrinjitapaul@gmail.com>
2024-02-28 00:19:39 +0530

    Update main.py

--- api/main.py
+++ api/main.py
@@ -1,14 +1,12 @@
 import json
-import os
+
 from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
 def home():
     return render_template('front.html')
 
-@app.route('/data')
 def get_data():
     with open('static/data/front.json', 'r') as f:
         data = json.load(f)
@@ -19,5 +17,11 @@ def get_data():
 
     return jsonify({'userImages': user_images, 'doctors': doctors})
 
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+# Function mapping for routes
+@app.route('/')
+def serve_home():
+    return home()
+
+@app.route('/data')
+def serve_data():
+    return get_data()
