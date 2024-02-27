76a3e3d3853acd110eda8947ce457757a5339781
Shrinjita Paul <113103022+Shrinjita@users.noreply.github.com>
2024-02-27 10:55:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,12 @@ def home():
 def get_data():
     with open('static/data/front.json', 'r') as f:
         data = json.load(f)
-    return jsonify(data)
+
+    # Access specific keys using the get function
+    user_images = data.get('userImages')
+    doctors = data.get('doctors')
+
+    return jsonify({'userImages': user_images, 'doctors': doctors})
 
 if __name__ == '__main__':
     app.run(debug=True)
